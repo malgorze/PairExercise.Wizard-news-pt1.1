db8d37efdcc66a140eff462736509a0dcebe07b6
malgorze <gosia.popowska@gmail.com>
2021-08-19 00:54:13 -0500

    nn

--- app.js
+++ app.js
@@ -1,10 +1,73 @@
 const express = require("express");
+const volleyball = require("volleyball");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(volleyball);
+app.use(express.static("public"));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">
+            ${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  res.send(html);
 });
+
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>${post.content}</p>
+          <small class="news-info">
+          ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+const PORT = 1338;
+
+app.listen(PORT, () => console.log(`App listening in port ${PORT}`));
